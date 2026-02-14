738de91dea7c7600adf8ed5f2e5cf1274afa01c3
yyubin <hazing120@gmail.com>
2026-02-14 12:27:00 +0900

    add vercel analytics

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { mount } from 'svelte'
+import { inject } from '@vercel/analytics'
 import './app.css'
 import App from './App.svelte'
+
+inject()
 
 const app = mount(App, {
   target: document.getElementById('app'),
